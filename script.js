0733d38f69cb37e51ef21659ec44306db072948d
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-06-11 14:17:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
     function handleStatusSliderButtonsClick() {
-        /* define Variables Related To Status Slide*/
+        /* define Variables Related To The Status Slide */
         const STATUS_SLIDES_PARENT = document.querySelector(".status-slide");
 
         const SINGLE_STATUS_SLIDE = document.querySelector(".status");
@@ -13,11 +13,20 @@
 
         const NUMBER_OF_ALL_STATUS_SLIDES = ALL_STATUS_SLIDES.length;
 
+        const STATUS_SLIDE_PROGRESS_CONTAINER =
+            document.querySelector(".progress");
+
+        const PROGRESS_LEVEL = document.querySelector(".progress-level");
+
+        // BASIC CALCULATIONS AND VARIABLES NECCESSARY FOR SMOOTH STATUS SLIDE
+        const NUM_OF_VISIBLE_SLIDES = Math.floor(
+            STATUS_SLIDES_PARENT_WIDTH / SINGLE_STATUS_SLIDE_WIDTH
+        );
+
+        function handleStatusNextButtonClick() {}
+
+        function handleStatusPreviousButtonClick() {}
     }
-
-    function handleStatusNextButtonClick() {}
-
-    function handleStatusPreviousButtonClick() {}
 
     handleStatusSliderButtonsClick();
 })();

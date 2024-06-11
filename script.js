bcc79a251afb669a5d80cad3d49129dba56ebcad
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-06-11 14:07:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+(function () {
+    function handleStatusSliderButtonsClick() {
+        /* define Variables Related To Status Slide*/
+        const STATUS_SLIDES_PARENT = document.querySelector(".status-slide");
+
+        const SINGLE_STATUS_SLIDE = document.querySelector(".status");
+
+        const ALL_STATUS_SLIDES = document.querySelectorAll(".status");
+
+        const STATUS_SLIDES_PARENT_WIDTH = STATUS_SLIDES_PARENT.clientWidth;
+
+        const SINGLE_STATUS_SLIDE_WIDTH = SINGLE_STATUS_SLIDE.clientWidth;
+
+        const NUMBER_OF_ALL_STATUS_SLIDES = ALL_STATUS_SLIDES.length;
+
+    }
+
+    function handleStatusNextButtonClick() {}
+
+    function handleStatusPreviousButtonClick() {}
+
+    handleStatusSliderButtonsClick();
+})();
